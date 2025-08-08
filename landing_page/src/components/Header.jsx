23fdbd6ad90aab2340f410ded0bbcd1aa295f9d4
elmerurbina <[elmerurbina570@gmail.com]>
2025-08-08 10:49:04 -0600

    fixes

--- landing_page/src/components/Header.jsx
+++ landing_page/src/components/Header.jsx
@@ -4,7 +4,7 @@ import LanguageSelector from './LanguageSelector';
 export default function Header({ logoSrc, onLanguageChange, lang }) {
   return (
     <header className="header d-flex justify-content-between align-items-center px-4 py-3" style={{ backgroundColor: 'var(--deep-blue)', color: 'white' }}>
-      <img src={logoSrc} alt="SignBridge Logo" style={{ height: '50px' }} />
+      <img src={logoSrc} alt="SignBridge Logo" style={{ height: '50px', borderRadius: '40px' }} />
       <LanguageSelector onLanguageChange={onLanguageChange} currentLang={lang} />
     </header>
   );
